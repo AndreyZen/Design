9bd80fb35bc14844d501b8cbfe9d3d6aaf98d6ad
Андрей Чаплыгин <andrej.chaplygin.02@mail.ru>
2022-03-07 23:21:12 +0300

    Добавьте файлы проекта.

---
+++ Design/Classes/Supplier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design.Classes
+{
+    class Supplier
+    {
+        public float price { get; set; }
+        public string amount { get; set; }
+        public string date_publish { get; set; }
+        public string company { get; set; }
+        public string order_by { get; set; }
+        public string inn { get; set; }
+        public string ogrn { get; set; }
+        public string kpp { get; set; }
+        public string okpo { get; set; }
+        public string register_date { get; set; }
+        public int rating { get; set; }
+        public string address { get; set; }
+        public string kapital { get; set; }
+        public string fin_info { get; set; }
+        public string advantages { get; set; }
+        public object phone { get; set; }
+        public string email { get; set; }
+        public object web_site { get; set; }
+    }
+}
